48a82d59a64fa3662755e4cb4d759eda8e966efc
mtribbs <89516065+mtribbs@users.noreply.github.com>
2022-02-21 16:34:54 +0100

    added most of the content

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function removeActiveClass() {
 }
 
 console.log("Hello");
+
+// map
+
+// const localMap = document.querySelector("map");
+
+// // Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const studioLocation = { lat: -25.344, lng: 131.036 };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: studioLocation,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: studioLocation,
+//     map: map,
+//   });
+// }
