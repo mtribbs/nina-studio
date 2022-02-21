8932a74846f0a89a914fd7f2102aa33f04d4e355
mtribbs <89516065+mtribbs@users.noreply.github.com>
2022-02-21 14:04:42 +0100

    added content

---
+++ script.js
@@ -0,0 +1,16 @@
+// panels
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
